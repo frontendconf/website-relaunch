27a4ab48ec8b3717ad023ae8c8f73453fef3149f
Thomas Jaggi <thomas@responsive.ch>
2019-03-18 10:26:20 +0100

    add client-side loaded news

--- components/Content.js
+++ components/Content.js
@@ -3,6 +3,7 @@ import { withRouter } from "next/router";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
 import Markdown from "markdown-to-jsx";
+import NewsSummary from "./NewsSummary";
 
 const currentPageQuery = gql`
   query currentPage($slug: String!) {
@@ -40,19 +41,34 @@ const currentPageQuery = gql`
 
 export default withRouter(({ router: { query } }) => (
   <Query query={currentPageQuery} variables={{ slug: query.slug || "/" }}>
-    {({ loading, error, data }) => {
+    {({
+      loading,
+      error,
+      data: {
+        pageCollection: {
+          items: [currentPage]
+        }
+      }
+    }) => {
       if (error) return <ErrorMessage message="Error loading pages." />;
       if (loading) return <div>Loading</div>;
-
-      const currentPage = data.pageCollection.items[0];
-
-      return currentPage ? (
+      
+      return (
         <section>
-          <h1>{currentPage.title}</h1>
-          <p>{currentPage.lead}</p>
-          {currentPage.body ? <Markdown>{currentPage.body}</Markdown> : null}
+          {currentPage ? (
+            <>
+              <h1>{currentPage.title}</h1>
+              <p>{currentPage.lead}</p>
+              {currentPage.body ? (
+                <Markdown>{currentPage.body}</Markdown>
+              ) : null}
+              {currentPage.showNews ? <NewsSummary /> : null}
+            </>
+          ) : (
+            "404"
+          )}
         </section>
-      ) : null;
+      );
     }}
   </Query>
 ));
---
+++ components/NewsSummary.js
@@ -0,0 +1,49 @@
+import ErrorMessage from "./ErrorMessage";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+import Link from "next/link";
+
+const newsQuery = gql`
+  query {
+    newsCollection(limit: 3, order: date_DESC) {
+      items {
+        title
+        date
+        slug
+        sys {
+          id
+        }
+      }
+    }
+  }
+`;
+
+export default () => (
+  <Query query={newsQuery} skip={!process.browser}>
+    {({ loading, error, data }) => {
+      if (error) return <ErrorMessage message="Error loading pages." />;
+      if (loading) return <div>Loading</div>;
+      if (!data) return null;
+
+      const news = data.newsCollection.items;
+
+      return (
+        <div>
+          <h3>News</h3>
+          <ul>
+            {news.map(item => (
+              <li key={item.sys.id}>
+                <Link
+                  href={{ pathname: "/", query: { slug: item.slug } }}
+                  as={`/${item.slug}`}
+                >
+                  <a>{item.title}</a>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      );
+    }}
+  </Query>
+);
