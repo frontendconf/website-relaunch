520afdf77d775cdd02efc35c2871e8b1f3615a83
Beat Gebistorf <beat.gebistorf@webwings.ch>
2019-03-29 17:02:23 +0100

    Fix list key error

--- components/shared/Grid.js
+++ components/shared/Grid.js
@@ -2,7 +2,7 @@ export const Container = ({ children, wide = false }) => (
   <div className={`container ${wide ? "container--wide" : ""}`}>{children}</div>
 );
 
-export const Row = ({ children }) => <div className={"row"}>{children}</div>;
+export const Row = ({ children, key }) => <div className={"row"} key={key}>{children}</div>;
 
 export const Col = ({ className, children }) => (
   <div className={`col ${className ? className : ""}`}>{children}</div>
