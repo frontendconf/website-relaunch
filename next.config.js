1838411bfb9c4258bd0bee366ab5363b8ca32a17
Thomas Jaggi <thomas@responsive.ch>
2019-04-11 20:04:09 +0200

    Add IntersectionObserver polyfill

--- next.config.js
+++ next.config.js
@@ -1,2 +1,22 @@
 const withSass = require("@zeit/next-sass");
-module.exports = withSass();
+
+module.exports = withSass({
+  webpack(config) {
+    const originalEntry = config.entry;
+
+    config.entry = async () => {
+      const entries = await originalEntry();
+
+      if (
+        entries["main.js"] &&
+        !entries["main.js"].includes("./client/polyfills.js")
+      ) {
+        entries["main.js"].unshift("./client/polyfills.js");
+      }
+
+      return entries;
+    };
+
+    return config;
+  }
+});
