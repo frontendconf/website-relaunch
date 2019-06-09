b18e6232b597db7f795c15b58ab938799fe039c8
Thomas Jaggi <thomas@responsive.ch>
2019-06-09 18:46:56 +0200

    wip: hide schedule for now

--- components/Content.js
+++ components/Content.js
@@ -314,7 +314,7 @@ export default withRouter(({ router: { query } }) => {
                             <Sponsors details={true} />
                           )}
                           {currentPage.showWorkshops && <Workshops />}
-                          {currentPage.showSchedule && <Schedule />}
+                          {/* {currentPage.showSchedule && <Schedule />} */}
                         </div>
                       </Col>
                     </Row>
