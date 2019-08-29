dd485aa093626fbf4147cb269d252ce05c854050
Thomas Jaggi <thomas@responsive.ch>
2019-08-29 17:55:04 +0200

    feat: persist active schedule tab, fall back based on current day

--- components/Schedule.js
+++ components/Schedule.js
@@ -1,7 +1,9 @@
+import { Component } from "react";
 import PropTypes from "prop-types";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
 import ErrorMessage from "./ErrorMessage";
+import { withRouter } from "next/router";
 import Link from "next/link";
 import { Tabs, TabList, Tab, TabPanels, TabPanel } from "@reach/tabs";
 import Image from "./Image";
@@ -36,182 +38,253 @@ const scheduleQuery = gql`
   }
 `;
 
-const Schedule = ({ filterTag }) => (
-  <div className="schedule">
-    <Query query={scheduleQuery}>
-      {({ loading, error, data }) => {
-        if (error) return <ErrorMessage message="Error loading schedule" />;
-        if (loading) return <div>Loading</div>;
+class Schedule extends Component {
+  constructor(props) {
+    super(props);
 
-        // Destructuring needs to be done outside the arguments to prevent mapping errors
-        let {
-          collection: { items: talks }
-        } = data;
+    this.state = {
+      activeTab: 0
+    };
+  }
 
-        // Filter by tag
-        if (filterTag) {
-          talks = talks.filter(talk =>
-            talk.tagsCollection.items.find(tag => tag.title === filterTag)
-          );
-        }
+  componentDidMount() {
+    if (!window.localStorage) {
+      return;
+    }
 
-        // Create map of talks by day and time
-        const scheduleMap = talks.reduce((acc, talk) => {
-          const day = dateFormatter.formatDate(talk.date, "dddd, D MMM");
+    const storedTab = parseInt(localStorage.getItem("scheduleTab") || 0, 10);
 
-          acc[day] = acc[day] || {
-            title: day,
-            sort: new Date(talk.date).getTime(),
-            slots: []
-          };
+    if (storedTab) {
+      this.setState({
+        activeTab: storedTab
+      });
+    }
+  }
 
-          if (talk.fromTime) {
-            acc[day].slots[talk.fromTime] = acc[day].slots[talk.fromTime] || {
-              sort: parseInt(talk.fromTime.replace(/[^0-9.]/g, ""), 10),
-              title: talk.fromTime,
-              talks: []
-            };
-            acc[day].slots[talk.fromTime].talks.push(talk);
-          }
+  // Persist to both URL and localStorage
+  onTabChange(index) {
+    const { slug } = this.props.router.query;
 
-          return acc;
-        }, {});
+    this.props.router.replace(
+      {
+        pathname: "/",
+        query: { slug, day: index }
+      },
+      `/${slug}?day=${index}`,
+      {
+        shallow: true
+      }
+    );
 
-        // Build schedule by sorting data
-        const schedule = Object.values(scheduleMap)
-          .filter(data => data.title)
-          .map(data => {
-            data.slots = Object.values(data.slots)
-              .sort((a, b) => a.sort - b.sort)
-              .map(slot => {
-                // TODO: Figure out whether the sorting order is correct
-                slot.talks = slot.talks.sort(
-                  (a, b) => a.room.charCodeAt(0) - b.room.charCodeAt(0)
-                );
+    if (window.localStorage) {
+      localStorage.setItem("scheduleTab", index);
+    }
 
-                return slot;
-              });
+    this.setState({
+      activeTab: index
+    });
+  }
 
-            return data;
-          })
-          .sort((a, b) => a.sort - b.sort);
+  render() {
+    return (
+      <div className="schedule">
+        <Query query={scheduleQuery}>
+          {({ loading, error, data }) => {
+            if (error) return <ErrorMessage message="Error loading schedule" />;
+            if (loading) return <div>Loading</div>;
 
-        return (
-          <Tabs className="schedule__tabs">
-            <TabList className="schedule__tablist">
-              {schedule.map((day, i) => (
-                <Tab key={i} className="schedule__tablist-item">
-                  {day.title}
-                </Tab>
-              ))}
-            </TabList>
-            <ul className="schedule__legend" role="presentation">
-              <li className="schedule__legend-item schedule__legend-item--Folium">
-                Lower floor
-              </li>
-              <li className="schedule__legend-item schedule__legend-item--Papiersaal">
-                Upper floor
-              </li>
-            </ul>
-            <TabPanels className="schedule__tabpanels">
-              {schedule.map((day, i) => (
-                <TabPanel key={i}>
-                  <div className="schedule__day">
-                    <h2 className="schedule__title visuallyhidden">
+            // Destructuring needs to be done outside the arguments to prevent mapping errors
+            let {
+              collection: { items: talks }
+            } = data;
+
+            // Filter by tag
+            if (this.props.filterTag) {
+              talks = talks.filter(talk =>
+                talk.tagsCollection.items.find(
+                  tag => tag.title === this.props.filterTag
+                )
+              );
+            }
+
+            // Create map of talks by day and time
+            const scheduleMap = talks.reduce((acc, talk) => {
+              const day = dateFormatter.formatDate(talk.date, "dddd, D MMM");
+
+              acc[day] = acc[day] || {
+                title: day,
+                sort: new Date(talk.date).getTime(),
+                slots: []
+              };
+
+              if (talk.fromTime) {
+                acc[day].slots[talk.fromTime] = acc[day].slots[
+                  talk.fromTime
+                ] || {
+                  sort: parseInt(talk.fromTime.replace(/[^0-9.]/g, ""), 10),
+                  title: talk.fromTime,
+                  talks: []
+                };
+                acc[day].slots[talk.fromTime].talks.push(talk);
+              }
+
+              return acc;
+            }, {});
+
+            // Build schedule by sorting data
+            const schedule = Object.values(scheduleMap)
+              .filter(data => data.title)
+              .map(data => {
+                data.slots = Object.values(data.slots)
+                  .sort((a, b) => a.sort - b.sort)
+                  .map(slot => {
+                    // TODO: Figure out whether the sorting order is correct
+                    slot.talks = slot.talks.sort(
+                      (a, b) => a.room.charCodeAt(0) - b.room.charCodeAt(0)
+                    );
+
+                    return slot;
+                  });
+
+                return data;
+              })
+              .sort((a, b) => a.sort - b.sort);
+
+            // Select active tab, either via URL or localStorage or current date
+            const queryTab = parseInt(this.props.router.query.day || 0, 10);
+            const currentDayTab = schedule.findIndex(
+              day =>
+                day.title ===
+                dateFormatter.formatDate(new Date(), "dddd, D MMM")
+            );
+            const activeTab = queryTab || this.state.activeTab || currentDayTab;
+
+            return (
+              <Tabs
+                className="schedule__tabs"
+                index={activeTab}
+                onChange={index => this.onTabChange(index)}
+              >
+                <TabList className="schedule__tablist">
+                  {schedule.map((day, i) => (
+                    <Tab key={i} className="schedule__tablist-item">
                       {day.title}
-                    </h2>
-                    <table className="schedule__slots">
-                      <tbody>
-                        {day.slots.map((slot, ii) => (
-                          <tr className="schedule__slot" key={ii} role="row">
-                            <th
-                              className="schedule__slot-time"
-                              role="rowheader"
-                            >
-                              {slot.title}
-                            </th>
-                            {slot.talks.map((talk, iii) => (
-                              <td
-                                className="schedule__slot-talk"
-                                key={iii}
-                                colSpan={slot.talks.length === 1 ? 2 : null}
-                                role="column"
+                    </Tab>
+                  ))}
+                </TabList>
+                <ul className="schedule__legend" role="presentation">
+                  <li className="schedule__legend-item schedule__legend-item--Folium">
+                    Lower floor
+                  </li>
+                  <li className="schedule__legend-item schedule__legend-item--Papiersaal">
+                    Upper floor
+                  </li>
+                </ul>
+                <TabPanels className="schedule__tabpanels">
+                  {schedule.map((day, i) => (
+                    <TabPanel key={i}>
+                      <div className="schedule__day">
+                        <h2 className="schedule__title visuallyhidden">
+                          {day.title}
+                        </h2>
+                        <table className="schedule__slots">
+                          <tbody>
+                            {day.slots.map((slot, ii) => (
+                              <tr
+                                className="schedule__slot"
+                                key={ii}
+                                role="row"
                               >
-                                {talk.speaker ? (
-                                  <Link
-                                    href={{
-                                      pathname: "/",
-                                      query: {
-                                        category: "speakers",
-                                        slug: talk.speaker.slug,
-                                        referrer: "schedule"
-                                      }
-                                    }}
-                                    as={`/speakers/${talk.speaker.slug}`}
+                                <th
+                                  className="schedule__slot-time"
+                                  role="rowheader"
+                                >
+                                  {slot.title}
+                                </th>
+                                {slot.talks.map((talk, iii) => (
+                                  <td
+                                    className="schedule__slot-talk"
+                                    key={iii}
+                                    colSpan={slot.talks.length === 1 ? 2 : null}
+                                    role="column"
                                   >
-                                    <a className="schedule__item">
-                                      <Image
-                                        className="schedule__item-photo"
-                                        src={`${talk.speaker.photo.url}`}
-                                      />
-                                      <span className="schedule__item-content">
+                                    {talk.speaker ? (
+                                      <Link
+                                        href={{
+                                          pathname: "/",
+                                          query: {
+                                            category: "speakers",
+                                            slug: talk.speaker.slug,
+                                            referrer: "schedule"
+                                          }
+                                        }}
+                                        as={`/speakers/${talk.speaker.slug}`}
+                                      >
+                                        <a className="schedule__item">
+                                          <Image
+                                            className="schedule__item-photo"
+                                            src={`${talk.speaker.photo.url}`}
+                                          />
+                                          <span className="schedule__item-content">
+                                            <span
+                                              className={`schedule__item-title schedule__item-title--talk schedule__item-title--${talk.room}`}
+                                            >
+                                              {talk.speaker.name}
+                                            </span>
+                                            <span className="schedule__item-talk">
+                                              {talk.title}
+                                            </span>
+                                          </span>
+                                        </a>
+                                      </Link>
+                                    ) : (
+                                      <span className="schedule__item">
                                         <span
-                                          className={`schedule__item-title schedule__item-title--talk schedule__item-title--${talk.room}`}
+                                          className={`schedule__item-title schedule__item-title--${talk.room}`}
                                         >
-                                          {talk.speaker.name}
+                                          {talk.detailPage ? (
+                                            <Link
+                                              href={{
+                                                pathname: "/",
+                                                query: {
+                                                  slug: talk.detailPage.slug
+                                                }
+                                              }}
+                                              as={`/${talk.detailPage.slug}`}
+                                            >
+                                              <a className="schedule__item-title schedule__item-title--custom">
+                                                {talk.title}
+                                              </a>
+                                            </Link>
+                                          ) : (
+                                            talk.title
+                                          )}
                                         </span>
-                                        <span className="schedule__item-talk">
-                                          {talk.title}
+                                        <span
+                                          className={`schedule__item-room schedule__item-room--${talk.room}`}
+                                        >
+                                          {talk.room}
                                         </span>
                                       </span>
-                                    </a>
-                                  </Link>
-                                ) : (
-                                  <span className="schedule__item">
-                                    <span
-                                      className={`schedule__item-title schedule__item-title--${talk.room}`}
-                                    >
-                                      {talk.detailPage ? (
-                                        <Link
-                                          href={{
-                                            pathname: "/",
-                                            query: {
-                                              slug: talk.detailPage.slug
-                                            }
-                                          }}
-                                          as={`/${talk.detailPage.slug}`}
-                                        >
-                                          <a className="schedule__item-title schedule__item-title--custom">
-                                            {talk.title}
-                                          </a>
-                                        </Link>
-                                      ) : (
-                                        talk.title
-                                      )}
-                                    </span>
-                                    <span
-                                      className={`schedule__item-room schedule__item-room--${talk.room}`}
-                                    >
-                                      {talk.room}
-                                    </span>
-                                  </span>
-                                )}
-                              </td>
+                                    )}
+                                  </td>
+                                ))}
+                              </tr>
                             ))}
-                          </tr>
-                        ))}
-                      </tbody>
-                    </table>
-                  </div>
-                </TabPanel>
-              ))}
-            </TabPanels>
-          </Tabs>
-        );
-      }}
-    </Query>
-  </div>
-);
+                          </tbody>
+                        </table>
+                      </div>
+                    </TabPanel>
+                  ))}
+                </TabPanels>
+              </Tabs>
+            );
+          }}
+        </Query>
+      </div>
+    );
+  }
+}
 
 Schedule.propTypes = {
   filterTag: PropTypes.string
@@ -221,4 +294,4 @@ Schedule.defaultProps = {
   filterTag: "FEC19"
 };
 
-export default Schedule;
+export default withRouter(Schedule);
