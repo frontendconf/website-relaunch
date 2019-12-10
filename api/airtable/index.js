0d5f4920c079c25844fc384c13174a4e152524dc
Thomas Jaggi <thomas@responsive.ch>
2019-12-10 20:45:44 +0100

    feat: submit form to airtable

--- api/airtable/index.js
+++ api/airtable/index.js
@@ -1,4 +1,4 @@
-var Airtable = require("airtable");
+const Airtable = require("airtable");
 
 const {
   AIRTABLE_API_KEY,
@@ -8,14 +8,68 @@ const {
 
 const base = new Airtable({ apiKey: AIRTABLE_API_KEY }).base(AIRTABLE_BASE);
 
+const fieldConfig = [
+  { name: "Title", type: "text" },
+  { name: "Summary", type: "textarea" },
+  {
+    name: "Level",
+    type: "select",
+    options: ["Easy", "Intermediate", "Advanced"]
+  },
+  {
+    name: "Topic",
+    type: "select",
+    options: [
+      "Tech",
+      "Design and/or UX",
+      "Management and/or strategy",
+      "Cross-disciplinary",
+      "Other"
+    ]
+  },
+  { name: "First name", type: "text" },
+  { name: "Last name", type: "text" },
+  { name: "Job title", type: "text" },
+  { name: "Company", type: "text" },
+  { name: "Country", type: "text" },
+  { name: "Bio", type: "textarea" },
+  { name: "Photo", type: "file", description: "URL" },
+  { name: "Experience", type: "checkbox", value: "true" },
+  { name: "Experience details", type: "textarea" },
+  { name: "E-mail", type: "email" },
+  { name: "Twitter", type: "text" },
+  { name: "LinkedIn", type: "text" },
+  { name: "Website", type: "text" },
+  { name: "Notes", type: "textarea" }
+];
+
 module.exports = async (req, res) => {
   res.setHeader("content-type", "application/json");
 
   if (req.method === "POST") {
     try {
+      const fields = Object.entries(req.body).reduce((acc, [key, value]) => {
+        if (value) {
+          const config = fieldConfig.find(field => field.name === key);
+
+          if (!config) {
+            return acc;
+          }
+
+          if (config.type === "file") {
+            acc[key] = [{ url: value }];
+          } else {
+            acc[key] = value;
+          }
+        }
+
+        return acc;
+      }, {});
+
+      // Save to Airtable
       await base(AIRTABLE_TABLE).create([
         {
-          fields: req.body
+          fields
         }
       ]);
 
@@ -34,40 +88,7 @@ module.exports = async (req, res) => {
   }
 
   const response = JSON.stringify({
-    fields: [
-      { name: "Title", type: "text" },
-      { name: "Summary", type: "textarea" },
-      {
-        name: "Level",
-        type: "select",
-        options: ["Easy", "Intermediate", "Advanced"]
-      },
-      {
-        name: "Topic",
-        type: "select",
-        options: [
-          "Tech",
-          "Design and/or UX",
-          "Management and/or strategy",
-          "Cross-disciplinary",
-          "Other"
-        ]
-      },
-      { name: "First name", type: "text" },
-      { name: "Last name", type: "text" },
-      { name: "Job title", type: "text" },
-      { name: "Company", type: "text" },
-      { name: "Country", type: "text" },
-      { name: "Bio", type: "textarea" },
-      { name: "Photo", type: "file" },
-      { name: "Experience", type: "checkbox", value: "true" },
-      { name: "Experience details", type: "textarea" },
-      { name: "E-mail", type: "email" },
-      { name: "Twitter", type: "text" },
-      { name: "LinkedIn", type: "text" },
-      { name: "Website", type: "text" },
-      { name: "Notes", type: "textarea" }
-    ]
+    fields: fieldConfig
   });
 
   return res.end(response);
--- components/Form.js
+++ components/Form.js
@@ -6,6 +6,7 @@ class Form extends Component {
 
     this.state = {
       isLoading: false,
+      isSuccess: false,
       message: null
     };
 
@@ -23,67 +24,98 @@ class Form extends Component {
 
     const response = await fetch(this.props.action, {
       method: this.props.method,
-      body: JSON.stringify({
-        fields: Object.fromEntries(formData)
-      })
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(Object.fromEntries(formData))
     });
     const data = await response.json();
 
     this.setState({
       message: data.message,
+      isSuccess: response.status < 400,
       isLoading: false
     });
   }
 
   render() {
-    return this.state.message ? (
-      <div>{this.state.message}</div>
-    ) : (
-      <form
-        action={this.props.action}
-        className="form"
-        method={this.props.method}
-        onSubmit={this.handleSubmit}
-      >
-        <dl>
-          {this.props.fields.map((field, i) => {
-            let element;
+    return (
+      <div className="form">
+        {this.state.message && (
+          <div className="form__message">{this.state.message}</div>
+        )}
+        {this.state.isSuccess ? null : (
+          <form
+            action={this.props.action}
+            method={this.props.method}
+            onSubmit={this.handleSubmit}
+          >
+            <dl>
+              {this.props.fields.map((field, i) => {
+                let element;
 
-            switch (field.type) {
-              case "select":
-                element = (
-                  <select name={field.name} id={field.name}>
-                    {field.options.map((option, ii) => (
-                      <option key={ii}>{option}</option>
-                    ))}
-                  </select>
+                switch (field.type) {
+                  case "select":
+                    element = (
+                      <select
+                        name={field.name}
+                        id={field.name}
+                        disabled={this.state.isLoading}
+                        aria-describedby={
+                          field.description ? `${field.name}-desc` : null
+                        }
+                      >
+                        {field.options.map((option, ii) => (
+                          <option key={ii}>{option}</option>
+                        ))}
+                      </select>
+                    );
+                    break;
+                  case "textarea":
+                    element = (
+                      <textarea
+                        name={field.name}
+                        id={field.name}
+                        disabled={this.state.isLoading}
+                        aria-describedby={
+                          field.description ? `${field.name}-desc` : null
+                        }
+                      />
+                    );
+                    break;
+                  default:
+                    element = (
+                      <input
+                        type={field.type === "file" ? "text" : field.type}
+                        name={field.name}
+                        id={field.name}
+                        value={field.value}
+                        disabled={this.state.isLoading}
+                        aria-describedby={
+                          field.description ? `${field.name}-desc` : null
+                        }
+                      />
+                    );
+                }
+                return (
+                  <React.Fragment key={i}>
+                    <dt>
+                      <label htmlFor={field.name}>{field.name}</label>
+                    </dt>
+                    <dd>
+                      {element}
+                      {field.description && (
+                        <div id={`${field.name}-desc`}>{field.description}</div>
+                      )}
+                    </dd>
+                  </React.Fragment>
                 );
-                break;
-              case "textarea":
-                element = <textarea name={field.name} id={field.name} />;
-                break;
-              default:
-                element = (
-                  <input
-                    type={field.type}
-                    name={field.name}
-                    id={field.name}
-                    value={field.value}
-                  />
-                );
-            }
-            return (
-              <React.Fragment key={i}>
-                <dt>
-                  <label htmlFor={field.name}>{field.name}</label>
-                </dt>
-                <dd>{element}</dd>
-              </React.Fragment>
-            );
-          })}
-        </dl>
-        <button type="submit">Submit</button>
-      </form>
+              })}
+            </dl>
+            <button type="submit">Submit</button>
+          </form>
+        )}
+      </div>
     );
   }
 }
