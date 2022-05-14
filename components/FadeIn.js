9ade77d1cb70c6232097768a508434290b24f54c
Christian Sany <christian@sany.ch>
2022-05-15 01:22:51 +0200

    Remove lazy render when reduced motion is prefered

--- components/FadeIn.js
+++ components/FadeIn.js
@@ -1,18 +1,25 @@
 import ScrollAnimation from "react-animate-on-scroll";
 import PropTypes from "prop-types";
 
-const FadeIn = ({ delay, children, style }) => (
-  <ScrollAnimation
-    animateIn="fadeInUp"
-    duration={0.7}
-    delay={delay}
-    style={style}
-    offset={100}
-    animateOnce={true}
-  >
-    {children}
-  </ScrollAnimation>
-);
+const FadeIn = ({ delay, children, style }) => {
+  const reducedMotion =
+    typeof window !== "undefined" &&
+    window.matchMedia("(prefers-reduced-motion)").matches;
+
+  return (
+    <ScrollAnimation
+      animateIn="fadeInUp"
+      duration={0.7}
+      delay={reducedMotion ? 0 : delay}
+      style={style}
+      offset={100}
+      animateOnce={true}
+      initiallyVisible={!!reducedMotion}
+    >
+      {children}
+    </ScrollAnimation>
+  );
+};
 
 FadeIn.propTypes = {
   delay: PropTypes.number,
